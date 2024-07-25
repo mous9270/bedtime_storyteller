534112dde716ad54ab033ccb1260056b052ef21d
mous9270 <mous9270222@gmail.com>
2024-07-25 05:34:37 -0400

    Added sever.js.                                             It seems useNewUrlParser, useUnifiedTopology are depriciated, needs to be updated.

---
+++ my-app/server.js
@@ -0,0 +1,23 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/readingApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.once('open', () => console.log('Connected to MongoDB'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
